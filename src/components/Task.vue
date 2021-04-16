9f506aa75d8b407f38c2a5ca6ad7819c4a4123ab
Giedre Klimaite <giedre.klima@gmail.com>
2021-04-16 15:29:35 +0300

    init

---
+++ src/components/Task.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="task">
+    <h3>
+      {{ task.title }}
+      <i @click="$emit('delete-task', task.id)" class="fas fa-times"></i>
+    </h3>
+    <p>{{ task.author }}</p>
+    <p>{{ task.day }}</p>
+    <p>{{ task.text }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Task",
+  props: {
+    task: Object,
+  },
+};
+</script>
+
+<style scope>
+.fas {
+  color: #8a8f8a;
+}
+
+.task {
+  background: #e9f0e9;
+  margin: 5px;
+  padding: 10px 20px;
+  cursor: pointer;
+}
+
+.task.reminder {
+  border-left: 5px solid #008000;
+}
+
+.task h3 {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+</style>
---
+++ src/pages/EditPost.vue
@@ -0,0 +1,7 @@
+<template>
+  <h1>Edit Post</h1>
+</template>
+
+<script>
+export default {};
+</script>
